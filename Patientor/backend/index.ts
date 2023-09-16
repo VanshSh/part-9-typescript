7feda0ecccf77e71f664938cbf80e12158d6512a
VanshSh <vanshsharma9354@gmail.com>
2023-09-16 13:44:34 +0530

    feat: :sparkles: Exercise Patientor backend, step2

--- Patientor/backend/index.ts
+++ Patientor/backend/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+const cors = require('cors')
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
-
+const PORT = 3001;
+app.use(cors());
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
